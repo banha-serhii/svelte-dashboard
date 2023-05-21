4087d33b7c3819f7e60916359e2127d6f6a77edd
serhi <serhij.banha@gmail.com>
2023-05-22 00:48:26 +0300

    First commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+  import "../app.css";
+</script>
+<svelte:head>
+  <title>Dashboard</title>
+</svelte:head>
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+  import LogoIcon from "../components/IconLogo.svelte";
+  import IconAccount from "../components/IconAccount.svelte";
+  import IconSettings from "../components/IconSettings.svelte";
+</script>
+
+<!-- component -->
+<aside class="flex">
+  <div class="flex rounded rounded-l-none  flex-col items-center w-16 h-screen py-8 space-y-8 bg-white dark:bg-gray-900 dark:border-gray-700">
+    <a href={"#"}>
+     <LogoIcon />
+    </a>
+
+    <a href={"#"} class="p-1.5 text-gray-500 focus:outline-nones transition-colors duration-200 rounded-lg dark:text-gray-400 dark:hover:bg-gray-800 hover:bg-gray-100">
+    <IconAccount />
+    </a>
+
+    <a href={"#"} class="p-1.5 text-gray-500 focus:outline-nones transition-colors duration-200 rounded-lg dark:text-gray-400 dark:hover:bg-gray-800 hover:bg-gray-100">
+    <IconSettings />
+    </a>
+  </div>
+</aside>
+
+<style lang="postcss">
+    :global(html) {
+        background-color:  #13132E;
+    }
+</style>
